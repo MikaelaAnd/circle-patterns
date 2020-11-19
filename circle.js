6ff115eb57752829e81ae7cf1c9fa90a8052532b
Mikaela Andersson <mikaelaaand@gmail.com>
2020-11-19 15:09:09 +0100

    made lines between dots

--- circle.js
+++ circle.js
@@ -3,7 +3,7 @@ window.addEventListener('load', main);
 function main() {
     const canvas = document.querySelector('canvas');
     const context = canvas.getContext('2d');
-    setFullScreen(canvas);
+    setFullscreen(canvas);
     draw(context);
 }
 
@@ -13,16 +13,15 @@ function draw(context) {
     const multiplier = 2;
 
     drawCircle(context, radius);
-    drawDotsOnCircle(context, radius, nrOfDots);
-    //drawLinesBetweenDots(context);
+    const dots = drawDotsOnCircle(context, radius, nrOfDots);
+    drawLinesBetweenDots(context, multiplier, dots);
 }
 
 /**
+ * Ritar ut en cirkel på skärmen
  * @param {CanvasRenderingContext2D} context 
- * @param {Number} radius
- * @param {Number} nrOfDots
+ * @param {Number} radius 
  */
-
 function drawCircle(context, radius) {
     context.strokeStyle = 'lightgrey';
     const centerX = window.innerWidth / 2;
@@ -34,37 +33,66 @@ function drawCircle(context, radius) {
 
     context.stroke();
 }
+
 /**
  * Ritar ett antal punker på cirkeln
  * @param {CanvasRenderingContext2D} context 
  * @param {Number} radius 
- * @param {Number} nrOfDots 
+ * @param {Number} nrOfDots
+ * @returns {Array<any>} the positions of all the dots
  */
-
 function drawDotsOnCircle(context, radius, nrOfDots) {
     context.fillStyle = 'red';
     const centerX = window.innerWidth / 2
     const centerY = window.innerHeight / 2
-    const dotRadius = radius / 30;
+    const dotRadius = radius / 100;
 
     // Formler baserat på "Enhetscirkeln"
     // radius * cos(angle) = x
     // radius * sin(angle) = y
 
+    const dots = [];
+
     for (let i = 0; i < nrOfDots; i++) {
         const angle = Math.PI * 2 / nrOfDots * i;
         const x = centerX + radius * Math.cos(angle);
         const y = centerY + radius * Math.sin(angle);
-        console.log(i, x, y);
+
+        dots.push({ x, y });
+        
         context.beginPath();
         context.arc(x, y, dotRadius, 0, Math.PI * 2);
         context.closePath();
         context.fill();
     }
-
+    
+    return dots;
 }
 
-function setFullScreen(canvas) {
+/**
+ * Ritar linjer mellan punkerna
+ * @param {CanvasRenderingContext2D} context 
+ * @param {Number} multiplier
+ * @param {Array<any>} dots
+ */
+function drawLinesBetweenDots(context, multiplier, dots) {
+    context.strokeStyle = 'black';
+    context.lineWidth = 2;
+
+    context.beginPath();
+    for (let i = 0; i < dots.length; i++) {
+        const dot = dots[i];
+        const nextIndex = (i * multiplier) % dots.length;
+        const nextDot = dots[nextIndex];
+        
+        context.moveTo(dot.x, dot.y);
+        context.lineTo(nextDot.x, nextDot.y);
+    }
+    context.stroke();
+}
+
+
+function setFullscreen(canvas) {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 }
